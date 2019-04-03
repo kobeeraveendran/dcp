4d9fe41ec26150c422ac9f50cda33249c1a030a0
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-04-03 12:27:33 -0400

    added solution and test cases w/ outputs for problem 21

---
+++ solutions/problem21.py
@@ -0,0 +1,23 @@
+def room_scheduler(time_intervals):
+
+    all_times = set()
+    num_rooms = 0
+
+    for lecture in time_intervals:
+        if lecture[0] in all_times or lecture[1] in all_times:
+            num_rooms += 1
+
+        all_times.update(range(lecture[0], lecture[1] + 1))            
+
+    return num_rooms
+
+test1 = [(30, 75), (0, 50), (60, 150)]
+test2 = [(0, 10), (5, 10), (3, 4)]
+test3 = [(0, 10), (5, 10), (3, 4), (3, 10)]
+test4 = [(0, 10), (5, 10), (3, 4), (3, 10), (11, 12)]
+
+
+print(room_scheduler(test1))            # expected output: 2
+print(room_scheduler(test2))            # expected output: 2
+print(room_scheduler(test3))            # expected output: 3
+print(room_scheduler(test4))            # expected output: 3
