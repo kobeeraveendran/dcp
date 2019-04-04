33bc8035205857881c051a35c13e1b24aded3b2b
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-04-04 12:41:57 -0400

    added test case with more words in output sentence than input set

--- solutions/problem22.py
+++ solutions/problem22.py
@@ -1,3 +1,5 @@
+# note: assumes that the input string and the dictionary/set of strings all use 
+# lowercase strings
 def reconstruct_sentence(words: set, sentence: str):
 
     curr_word = ''
@@ -10,10 +12,14 @@ def reconstruct_sentence(words: set, sentence: str):
             reconstructed_sentence.append(curr_word)
             curr_word = ''
 
-    return reconstructed_sentence
+    return reconstructed_sentence if len(reconstructed_sentence) > 0 else None
 
+# could also use dictionaries here (as the problem states), but sets are more suited
+# since there isn't a value
 test1 = (set(['quick', 'brown', 'the', 'fox']), 'thequickbrownfox')
 test2 = (set(['bed', 'bath', 'and', 'beyond']), 'bedbathandbeyond')
+test3 = (set(['i', 'am', 'the', 'one', 'who', 'knocks', 'at', 'door']), 'iamtheonewhoknocksatthedoor')
 
 print(reconstruct_sentence(*test1))
 print(reconstruct_sentence(*test2))
+print(reconstruct_sentence(*test3))
