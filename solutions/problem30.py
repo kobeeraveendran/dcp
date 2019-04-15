9620459994b6ca7b553d68c72b1b9ca7f98a9fac
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-04-15 15:19:32 -0400

    created better method that accounts for edge case where huge troughs appear between the left and right extremes; passes all test cases

--- solutions/problem30.py
+++ solutions/problem30.py
@@ -5,8 +5,25 @@ def calculate_water_trapped(array):
 
     for wall in array:
         if wall < limit:
-            print('water gains: ', limit - wall)
             water += limit - wall
+
+    return water
+
+def calculate_water_trapped_complete(array):
+
+    cap = min(array[0], array[-1])
+    #max_left = array[0]
+    #max_right = array[-1]
+
+    water = 0
+
+    for i in range(1, len(array) - 1):
+        
+        if array[i - 1] > cap and array[i + 1] > cap:
+            limit = min(array[i - 1], array[i + 1])
+            water += limit - array[i] if array[i] < limit else 0
+        else:
+            water += cap - array[i] if array[i] < cap else 0
 
     return water
 
@@ -14,8 +31,18 @@ test1 = [2, 1, 2]
 test2 = [3, 0, 1, 3, 0, 5]
 test3 = [3, 0, 0, 2, 0, 4]
 test4 = [1, 2, 3, 2, 1]
+test5 = [2, 0, 5, 0, 4, 1, 3]
 
 print(calculate_water_trapped(test1))       # expected out: 1
 print(calculate_water_trapped(test2))       # expected out: 8
 print(calculate_water_trapped(test3))       # expected out: 10
 print(calculate_water_trapped(test4))       # expected out: 0
+print(calculate_water_trapped(test5))       # expected out: 8
+
+print('--------------')
+
+print(calculate_water_trapped_complete(test1))      # expected out: 1
+print(calculate_water_trapped_complete(test2))      # expected out: 8
+print(calculate_water_trapped_complete(test3))      # expected out: 10
+print(calculate_water_trapped_complete(test4))      # expected out: 0
+print(calculate_water_trapped_complete(test5))      # expected out: 8
