f69c625cd042c9c5af4e48686641623d86cacd4c
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-04-18 13:05:36 -0400

    added edge case and edited solution to address it

--- solutions/problem36.py
+++ solutions/problem36.py
@@ -7,6 +7,8 @@ class Node:
 
 def find_second_largest_node(root):
 
+    parent = root.left
+
     while root.right != None:
         parent = root
         root = root.right
@@ -14,7 +16,13 @@ def find_second_largest_node(root):
     return parent
 
 test1 = Node(4, Node(2, Node(1), Node(3)), Node(6, Node(5), Node(8)))
+
+# BST devolved into linked list going to the right
 test2 = Node(1, right = Node(2, right = Node(3, right = Node(4, right = Node(6)))))
+
+# BST devolved into linked list going to the left
+test3 = Node(7, left = Node(5, left = Node(4, left = Node(3, left = Node(2, left = Node(1))))))
 
 print(find_second_largest_node(test1).value)
 print(find_second_largest_node(test2).value)
+print(find_second_largest_node(test3).value)
