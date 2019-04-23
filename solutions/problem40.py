3d514d96dbdb624d1f13637734a3bc05224878ad
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-04-23 14:16:38 -0400

    added alternate solution to problem 40

--- solutions/problem40.py
+++ solutions/problem40.py
@@ -6,9 +6,27 @@ def find_single(array):
     for item in classes:
         if array.count(item) == 1:
             return item
+
+def find_single2(array):
+
+    frequencies = {}
+
+    for num in array:
+        if num in frequencies:
+            frequencies[num] += 1
+        else:
+            frequencies[num] = 1
+
+    for key, value in frequencies.items():
+        if value == 1:
+            return key
         
 test1 = [6, 1, 3, 3, 3, 6, 6]
 test2 = [13, 19, 13, 13]
 
 print(find_single(test1))   # expected out: 1
 print(find_single(test2))   # expected out: 19
+
+# same expected output as above
+print(find_single2(test1))
+print(find_single2(test2))
