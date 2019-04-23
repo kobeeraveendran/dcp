a6d0afb8ff76a9bb2b174bb461ef9343a94e0e68
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-04-23 12:31:35 -0400

    added problem and working solution to problem 41

---
+++ solutions/problem41.py
@@ -0,0 +1,49 @@
+def compute_itinerary(flight_list, start):
+
+    itinerary = [start]
+    origins = {}
+
+    # mapping of origins to destinations
+    for flight in flight_list:
+        if flight[0] in origins:
+            origins[flight[0]].append(flight[1])
+
+        else:
+            origins[flight[0]] = [flight[1]]
+
+    # lexicographically sort any keys with multiple values by value
+    for key in origins.keys():
+        if len(origins[key]) > 1:
+            origins[key].sort()
+
+    # DEBUG: printing origins dict
+    '''
+    for key in origins.keys():
+        print(key + ': ', end = '')
+        
+        for i in range(len(origins[key])):
+            print(origins[key][i], end = ' ')
+        print('\n')
+    '''
+
+    # generate itinerary
+    itinerary.append(origins[start][0])
+    origins[start].remove(origins[start][0])
+
+    for i in range(len(flight_list) - 1):
+        try:
+            itinerary.append(origins[itinerary[-1]][0])
+        except:
+            # this should only happen if the destination of one 
+            # origin isn't itself an origin
+            return None
+    
+    return itinerary
+
+test1 = [('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')]
+test2 = [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')]
+test3 = [('SFO', 'COM'), ('COM', 'YYZ')]
+
+print(compute_itinerary(test1, 'YUL'))
+print(compute_itinerary(test2, 'A'))
+print(compute_itinerary(test3, 'COM'))
